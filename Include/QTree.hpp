4afb4c38d30d3910021ba7b3735e7ee4e69207db
miguel <luizmiguel.pessotti@gmail.com>
2024-03-12 20:36:02 -0300

    Almost finished NN search for the QTree

--- Include/QTree.hpp
+++ Include/QTree.hpp
@@ -6,6 +6,149 @@
 #include <algorithm>
 
 namespace QTree {
+
+//     struct Contact {
+//     public:
+//         bool check_contact(const QuadTree* main_quadtree, const QuadTree* secondary_quadtree) {
+//             bool contact = false;
+//             contact = check_boundary(main_quadtree, secondary_quadtree);
+
+//             if (contact) {
+//                 if (this->direction == "left" || this->direction == "right") {
+//                     double start1 = main_quadtree->boundary.bottom;
+//                     double end1 = main_quadtree->boundary.top;
+//                     double start2 = secondary_quadtree->boundary.bottom;
+//                     double end2 = secondary_quadtree->boundary.top;
+
+//                     if(rangesOverlap(start1, end1, start2, end2)) {
+//                         return true;
+//                     }
+//                 } else if (this->direction == "top" || this->direction == "bottom") {
+//                     double start1 = main_quadtree->boundary.left;
+//                     double end1 = main_quadtree->boundary.right;
+//                     double start2 = secondary_quadtree->boundary.left;
+//                     double end2 = secondary_quadtree->boundary.right;
+
+//                     if(rangesOverlap(start1, end1, start2, end2)) {
+//                         return true;
+//                     }
+//                 }
+//             }
+//             return false;
+//         }
+
+//         std::string get_direction(){
+//             return this->direction;
+//             }
+
+//     private:
+//         std::string direction;
+//         QuadTree* quadtree;
+
+//         bool check_boundary(const QuadTree* main_quadtree, const QuadTree* secondary_quadtree) {
+//             if (main_quadtree->boundary.left == secondary_quadtree->boundary.right) {
+//                 this->direction = "left";
+//                 return true;
+//             } else if (main_quadtree->boundary.right == secondary_quadtree->boundary.left) {
+//                 this->direction = "right";
+//                 return true;
+//             } else if (main_quadtree->boundary.top == secondary_quadtree->boundary.bottom) {
+//                 this->direction = "top";
+//                 return true;
+//             } else if (main_quadtree->boundary.bottom == secondary_quadtree->boundary.top) {
+//                 this->direction = "bottom";
+//                 return true;
+//             } else {
+//                 return false;
+//             }
+//         }
+
+//         bool rangesOverlap(double start1, double end1, double start2, double end2) {
+//             return (start1 <= end2) && (start2 <= end1);
+//     }
+
+// };
+
+    // struct NearestNeighbor {
+    // public:
+    //     NearestNeighbor(QuadTree* main_quadtree) : main_quadtree(main_quadtree) {}
+
+    //     Point find_nearest_neighbor(const Point& point) {
+    //         Point nearest_point;
+
+    //         this->set_current_point(point);
+    //         this->set_current_quadtree();
+    //         this->set_closest_quadtrees();
+
+    //         return nearest_point;
+    //     }
+
+    // private:
+    //     QuadTree* main_quadtree;
+    //     QuadTree* current_quadtree;
+    //     Point current_point;
+    //     std::vector<const QuadTree*> closest_quadtrees;
+    //     std::vector<const QuadTree*> second_look_quadtrees; // Quadtrees that the closest quadtrees are empty
+
+    //     std::vector<double> get_current_distances(const QuadTree*& quadtree, const Point& point) const {
+    //         std::vector<double> distances;
+
+    //         distances.push_back(std::abs(quadtree->boundary.left - point.x));
+    //         distances.push_back(std::abs(quadtree->boundary.right - point.x));
+    //         distances.push_back(std::abs(quadtree->boundary.top - point.y));
+    //         distances.push_back(std::abs(quadtree->boundary.bottom - point.y));
+    //         return distances;
+    //     }
+
+    //     double get_distance(const Point& point, const QuadTree*& quadtree, const std::string& direction) const {
+    //         double distance;
+
+    //         if (direction == "left") {
+    //             distance = std::abs(quadtree->boundary.left - point.x);
+    //         } else if (direction == "right") {
+    //             distance = std::abs(quadtree->boundary.right - point.x);
+    //         } else if (direction == "top") {
+    //             distance = std::abs(quadtree->boundary.top - point.y);
+    //         } else if (direction == "bottom") {
+    //             distance = std::abs(quadtree->boundary.bottom - point.y);
+    //         } else {
+    //             throw std::invalid_argument("Invalid direction");
+    //         }
+    //         return distance;
+    //     }
+
+    //     QuadTree* find_quadtree(QuadTree*& quad_tree) const {
+    //         if (quad_tree->divided) {
+    //             if (quad_tree->northeast->boundary.contains(this->current_point)) {
+    //                 return find_quadtree(quad_tree->northeast);
+    //             } else if (quad_tree->northwest->boundary.contains(this->current_point)) {
+    //                 return find_quadtree(quad_tree->northwest);
+    //             } else if (quad_tree->southeast->boundary.contains(this->current_point)) {
+    //                 return find_quadtree(quad_tree->southeast);
+    //             } else if (quad_tree->southwest->boundary.contains(this->current_point)) {
+    //                 return find_quadtree(quad_tree->southwest);
+    //             }
+    //         }
+    //         return quad_tree;
+    //     }
+
+    //     // Setters
+
+    //     void set_current_quadtree() {
+    //         this->current_quadtree = find_quadtree(this->main_quadtree);
+    //     }
+
+    //     void set_current_point(const Point& point) {
+    //         this->current_point = point;
+    //     }
+
+    //     void set_closest_quadtrees(QuadTree* quad_tree) {
+
+    //         if
+            
+    //     }
+    // };
+
     class Point {
     public:
         double x, y;
@@ -35,11 +178,6 @@ namespace QTree {
             right = x + w / 2;
             top = y - h / 2;
             bottom = y + h / 2;
-            
-            top_left = Point(left, top);
-            top_right = Point(right, top);
-            bottom_left = Point(left, bottom);
-            bottom_right = Point(right, bottom);
         }
 
         bool contains(const Point& point) const {
@@ -54,32 +192,86 @@ namespace QTree {
     };
 
     class QuadTree {
-        public:
-            static const int DEFAULT_CAPACITY = 4;
-            static const int MAX_DEPTH = 8;
-            Rectangle boundary;
-            int capacity;
-            std::vector<Point> points;
-            bool divided;
-            QuadTree *northeast, *northwest, *southeast, *southwest;
-            QuadTree *parent;
-            QuadTree *me = this;
-            int depth;
-
-
-        QuadTree(const Rectangle& boundary, int capacity = DEFAULT_CAPACITY, int depth = 0, QuadTree *parent = nullptr)
-            : boundary(boundary), capacity(capacity), divided(false), depth(depth),
-            northeast(nullptr), northwest(nullptr), southeast(nullptr), southwest(nullptr), parent(parent) {
-                if (capacity < 1) {
-                    throw std::range_error("capacity must be greater than 0");
+    public:
+        QuadTree(const Rectangle& boundary, const int capacity = DEFAULT_CAPACITY)
+                : boundary(boundary), capacity(capacity), divided(false), depth(0),
+                northeast(nullptr), northwest(nullptr), southeast(nullptr), southwest(nullptr), parent(nullptr), position("root"), number_of_points(0) {
+                    if (capacity < 1) {
+                        throw std::range_error("capacity must be greater than 0");
+                    }
                 }
-            }
+        
+        QuadTree(const Rectangle& boundary, const int capacity = DEFAULT_CAPACITY, std::vector<Point> points)
+                : boundary(boundary), capacity(capacity), divided(false), depth(0),
+                northeast(nullptr), northwest(nullptr), southeast(nullptr), southwest(nullptr), parent(nullptr), position("root"), number_of_points(0) {
+                    if (capacity < 1) {
+                        throw std::range_error("capacity must be greater than 0");
+                    }
+
+                    for (const auto& point : points) {
+                        insert(point);
+                    }
+                }
 
         ~QuadTree() {
                 delete northeast;
                 delete northwest;
                 delete southeast;
                 delete southwest;
+            }
+
+        bool insert(const Point& point) {
+            if (!boundary.contains(point)) {
+                return false;
+            }
+
+            if (points.size() < capacity || depth == MAX_DEPTH) {
+                points.push_back(point);
+                this->number_of_points++;
+                return true;
+            }
+
+            if (!divided) {
+                subdivide();
+            }
+
+            points.push_back(point);
+            this->number_of_points++;
+
+            if (northeast->insert(point) || northwest->insert(point) || 
+                southeast->insert(point) || southwest->insert(point)) {
+                return true;
+            }
+
+            // This should never happen
+            return false;
+        }
+
+        Point nearest_neighbor(const Point& point) {
+            Point nearest_point;
+            return nearest_point;
+        }
+
+    private:
+        static const int DEFAULT_CAPACITY = 4;
+        static const int MAX_DEPTH = 8;
+        Rectangle boundary;
+        int capacity;
+        std::vector<Point> points;
+        int number_of_points;
+        bool divided;
+        QuadTree *northeast, *northwest, *southeast, *southwest;
+        QuadTree *parent;
+        std::string position;
+        int depth;
+
+
+        QuadTree(const Rectangle& boundary, const int capacity, int depth, QuadTree *parent, std::string position)
+            : boundary(boundary), capacity(capacity), divided(false), depth(depth),
+            northeast(nullptr), northwest(nullptr), southeast(nullptr), southwest(nullptr), parent(parent), position(position), number_of_points(0) {
+                if (capacity < 1) {
+                    throw std::range_error("capacity must be greater than 0");
+                }
             }
 
         void subdivide() {
@@ -93,44 +285,19 @@ namespace QTree {
             Rectangle seRect(x + w / 2, y + h / 2, w, h);
             Rectangle swRect(x - w / 2, y + h / 2, w, h);
 
-            northeast = new QuadTree(neRect, capacity, depth + 1, this);
-            northwest = new QuadTree(nwRect, capacity, depth + 1, this);
-            southeast = new QuadTree(seRect, capacity, depth + 1, this);
-            southwest = new QuadTree(swRect, capacity, depth + 1, this);
+            this->northeast = new QuadTree(neRect, capacity, depth + 1, this, "northeast");
+            this->northwest = new QuadTree(nwRect, capacity, depth + 1, this, "northwest");
+            this->southeast = new QuadTree(seRect, capacity, depth + 1, this, "southeast");
+            this->southwest = new QuadTree(swRect, capacity, depth + 1, this, "southwest");
 
             for (const auto& point : points) {
-                if (northeast->insert(point) || northwest->insert(point) || 
-                    southeast->insert(point) || southwest->insert(point)) {
+                if (this->northeast->insert(point) || this->northwest->insert(point) || 
+                    this->southeast->insert(point) || this->southwest->insert(point)) {
                     continue;
                 }
             }
 
             divided = true;
-        }
-
-        bool insert(const Point& point) {
-            if (!boundary.contains(point)) {
-                return false;
-            }
-
-            if (points.size() < capacity || depth == MAX_DEPTH) {
-                points.push_back(point);
-                return true;
-            }
-
-            if (!divided) {
-                subdivide();
-            }
-
-            points.push_back(point);
-
-            if (northeast->insert(point) || northwest->insert(point) || 
-                southeast->insert(point) || southwest->insert(point)) {
-                return true;
-            }
-
-            // This should never happen
-            return false;
         }
 
         std::vector<Point> query(const Rectangle& range, std::vector<Point>& found) const {
@@ -153,23 +320,61 @@ namespace QTree {
 
             return found;
         }
-    
-    private:
 
-        QuadTree* find_quadtree(const Point& point) {
-            if (divided) {
-                if (northeast->boundary.contains(point)) {
-                    return northeast->find_quadtree(point);
-                } else if (northwest->boundary.contains(point)) {
-                    return northwest->find_quadtree(point);
-                } else if (southeast->boundary.contains(point)) {
-                    return southeast->find_quadtree(point);
-                } else if (southwest->boundary.contains(point)) {
-                    return southwest->find_quadtree(point);
+        QuadTree* find_quadtree(QuadTree*& quad_tree, const Point& point) const {
+            if (quad_tree->divided) {
+                if (quad_tree->northeast->boundary.contains(point)) {
+                    return find_quadtree(quad_tree->northeast, point);
+                } else if (quad_tree->northwest->boundary.contains(point)) {
+                    return find_quadtree(quad_tree->northwest, point);
+                } else if (quad_tree->southeast->boundary.contains(point)) {
+                    return find_quadtree(quad_tree->southeast, point);
+                } else if (quad_tree->southwest->boundary.contains(point)) {
+                    return find_quadtree(quad_tree->southwest, point);
                 }
             }
-            return this;
+            return quad_tree;
         }
 
+        double find_parent_distance(const QuadTree* quadtree, const Point& point) const {
+
+            if (quadtree->parent->position == "root") {
+
+            } else {
+                if (quadtree->position == "northeast"){
+                    if (quadtree->parent->northwest->points.size() > 0) {
+                        return quadtree->parent->northwest->points[0].distanceFrom(point);
+                    } else if (quadtree->parent->southwest->points.size() > 0) {
+                        return quadtree->parent->southwest->points[0].distanceFrom(point);
+                    } else if (quadtree->parent->southeast->points.size() > 0) {
+                        return quadtree->parent->southeast->points[0].distanceFrom(point);
+                    }
+                } else if (quadtree->position == "northwest") {
+                    if (quadtree->parent->northeast->points.size() > 0) {
+                        return quadtree->parent->northeast->points[0].distanceFrom(point);
+                    } else if (quadtree->parent->southwest->points.size() > 0) {
+                        return quadtree->parent->southwest->points[0].distanceFrom(point);
+                    } else if (quadtree->parent->southeast->points.size() > 0) {
+                        return quadtree->parent->southeast->points[0].distanceFrom(point);
+                    }
+                } else if (quadtree->position == "southeast") {
+                    if (quadtree->parent->northeast->points.size() > 0) {
+                        return quadtree->parent->northeast->points[0].distanceFrom(point);
+                    } else if (quadtree->parent->southwest->points.size() > 0) {
+                        return quadtree->parent->southwest->points[0].distanceFrom(point);
+                    } else if (quadtree->parent->northwest->points.size() > 0) {
+                        return quadtree->parent->northwest->points[0].distanceFrom(point);
+                    }
+                } else if (quadtree->position == "southwest") {
+                    if (quadtree->parent->northeast->points.size() > 0) {
+                        return quadtree->parent->northeast->points[0].distanceFrom(point);
+                    } else if (quadtree->parent->northwest->points.size() > 0) {
+                        return quadtree->parent->northwest->points[0].distanceFrom(point);
+                    } else if (quadtree->parent->southeast->points.size() > 0) {
+                        return quadtree->parent->southeast->points[0].distanceFrom(point);
+                    }
+                }
+            }
+        }
     };
 }  // namespace QTree
