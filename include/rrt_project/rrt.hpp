133005dc8dd84131a8b07c18d5889a70684aa587
miguel <luizmiguel.pessotti@gmail.com>
2024-04-23 20:40:48 -0300

    Reorganized the Simple RRT in a class format

---
+++ include/rrt_project/rrt.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include "grid_map.hpp"
+#include "QTree.hpp"
+
+namespace motion_planning {
+    class Node {
+        public:
+            std::vector<double> position;
+            Node* parent;
+            int x;
+            int y;
+            Node(std::vector<double> pos, Node* par);
+    };
+
+    class RRT {
+        public:
+            RRT(const cv::Mat& map, Node* start, Node* goal, int num_nodes, double step_size, double goal_threshold, double bias_probability);
+            std::vector<Node*> run();
+            void plot(const std::vector<Node*>& nodes, const Node* start, const Node* end, bool reached);
+            std::vector<Node*> traceGoalPath(Node* goal_node);
+            virtual ~RRT();
+
+        private:
+            cv::Mat map_;
+            Node* start_;
+            Node* goal_;
+            int num_nodes_;
+            double step_size_;
+            double goal_threshold_;
+            double bias_probability_;
+
+            Node* biasedSample();
+            Node* steer(Node* nearest, const Node& sample) const;
+            double distance(const Node& node1, const Node& node2) const;
+            bool checkObstacleIntersection(const Node* start, const Node* end) const;
+    };
+} // namespace motion_planning
