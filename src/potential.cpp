ea2392f375e242a7af20769f69d776597c9fc2cb
Luiz Miguel Pessotti <luizmiguel.pessotti@gmail.com>
2024-09-02 14:30:54 -0300

    Added a constant to try in deform the potential on the x direction

--- src/potential.cpp
+++ src/potential.cpp
@@ -1,6 +1,7 @@
 #include "potential.hpp"
 #include <cmath>
 #include <stdexcept>
+#include <iostream>
 
 namespace potential {
     ObstacleAvoidance::ObstacleAvoidance(double n, double a, double b, double k, double lambda) : n(n), a(a), b(b), k(k), lambda(lambda) {
@@ -11,20 +12,32 @@ namespace potential {
     }
 
     double ObstacleAvoidance::calculate_V(double x_diff, double y_diff) {
-        return std::exp(-std::pow(x_diff, n)/a) * std::exp(-std::pow(y_diff, n)/b);
+        return std::exp(-std::pow(x_diff, n)/a) * std::exp(-std::pow(y_diff, n)/a_aux);
     }
 
     Eigen::Vector2d ObstacleAvoidance::j_ob(double v, double x_diff, double y_diff) {
         Eigen::Vector2d result;
         result(0) = -v * n * std::pow(x_diff, n - 1) / a;
-        result(1) = -v * n * std::pow(y_diff, n - 1) / b;
+        result(1) = -v * n * std::pow(y_diff, n - 1) / a_aux;
         return result;
     }
 
-    std::pair<double, double> ObstacleAvoidance::obstacle_avoidance(const Eigen::Vector2d& robot_point, const Eigen::Vector2d& obstacle_point) {
+    std::pair<double, double> ObstacleAvoidance::obstacle_avoidance(const Eigen::Vector2d& robot_point, const Eigen::Vector2d& obstacle_point, double gamma = 1.0) {
         double x_diff = robot_point(0) - obstacle_point(0);
         double y_diff = robot_point(1) - obstacle_point(1);
-    
+
+        if (gamma == 0) {
+            gamma = 0.0001;
+        } else if (gamma > 1) {
+            gamma = 1;
+            std::cout << "The auxiliar gain x direction is greater than 1, setting it to 1" << std::endl;
+        } else if (gamma < 0) {
+            gamma = 0.0001;
+            std::cout << "The auxiliar gain x direction is less than 0, setting it to 0.0001" << std::endl;
+        }
+
+        a_aux = b*gamma;
+
         double v = calculate_V(x_diff, y_diff);
         V = v;
 
@@ -35,12 +48,12 @@ namespace potential {
         Eigen::Matrix2d lambda_Job = lambda * Eigen::Matrix2d::Identity();
 
         Eigen::Matrix2d J_ob_T_J_ob = J_ob * J_ob.transpose() + lambda_Job;
-        Eigen::Vector2d pseudo_inv_term = J_ob.transpose() * J_ob_T_J_ob.inverse() * v_ref;
+        Eigen::Vector2d X_dot_obs = J_ob.transpose() * J_ob_T_J_ob.inverse() * v_ref;
 
-        double x_dot = pseudo_inv_term(0);
-        double y_dot = pseudo_inv_term(1);
+        double x_dot_obs = X_dot_obs(0);
+        double y_dot_obs = X_dot_obs(1);
 
-        return std::make_pair(x_dot, y_dot);
+        return std::make_pair(x_dot_obs, y_dot_obs);
     }
 
     Eigen::Vector2d ObstacleAvoidance::get_J() const {
