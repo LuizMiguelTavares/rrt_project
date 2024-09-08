8879e190f70aea80121174ede54bb7ef75797f46
Jetson_Xavier <luizmiguel.pessotti@gmail.com>
2024-09-08 19:04:38 -0300

    The local map was simplified and published on the map reference frame instead of the robot reference frame. The center of the local map is the control point of the robot

--- nodes/local_map.cpp
+++ nodes/local_map.cpp
@@ -1,39 +1,65 @@
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <tf/transform_listener.h>
 #include <tf/transform_datatypes.h>
 #include <cmath>
 
-class LocalMapNode {
+class LocalMapInMapFrameNode {
 public:
-    LocalMapNode() {
+    LocalMapInMapFrameNode() {
         ros::NodeHandle nh;
         ros::NodeHandle private_nh("~");
 
         // Parameters
         private_nh.param("global_map_topic", global_map_topic, std::string("/map"));
-        private_nh.param("local_map_topic", local_map_topic, std::string("local_map"));
+        private_nh.param("local_map_topic", local_map_topic, std::string("local_map_in_map_frame"));
         private_nh.param("pose_topic", pose_topic, std::string("/vrpn_client_node/P1/pose"));
         private_nh.param("width_meters", width_meters, 2.0);
         private_nh.param("height_meters", height_meters, 2.0);
         private_nh.param("local_resolution", local_resolution, 0.01);
         private_nh.param("publish_rate", publish_rate, 10);
+        private_nh.param("robot_frame", robot_frame, std::string("base_link"));
+        private_nh.param("map_frame", map_frame, std::string("map_frame_for_local_rrt"));
+        private_nh.param("control_point_offset", control_point_offset, 0.15);
 
         // Subscribers and Publishers
-        global_map_sub = nh.subscribe(global_map_topic, 1, &LocalMapNode::globalMapCallback, this);
-        pose_sub = nh.subscribe(pose_topic, 1, &LocalMapNode::poseCallback, this);
+        global_map_sub = nh.subscribe(global_map_topic, 1, &LocalMapInMapFrameNode::globalMapCallback, this);
+        pose_sub = nh.subscribe(pose_topic, 1, &LocalMapInMapFrameNode::poseCallback, this);
         local_map_pub = nh.advertise<nav_msgs::OccupancyGrid>(local_map_topic, 10);
+
+        // Initialize the tf listener
+        tf_listener = new tf::TransformListener();
 
         // Main loop
         ros::Rate rate(publish_rate);
         while (ros::ok()) {
-            publishLocalMap();
+            publishLocalMapInMapFrame();
             ros::spinOnce();
             rate.sleep();
         }
     }
 
+    ~LocalMapInMapFrameNode() {
+        delete tf_listener;
+    }
+
 private:
+    ros::Subscriber global_map_sub;
+    ros::Subscriber pose_sub;
+    ros::Publisher local_map_pub;
+
+    nav_msgs::OccupancyGrid global_map;
+    geometry_msgs::PoseStamped current_pose;
+    double global_resolution, control_point_offset;
+    int width_cells, height_cells;
+
+    std::string global_map_topic, local_map_topic, pose_topic, robot_frame, map_frame;
+    double width_meters, height_meters, local_resolution;
+    int publish_rate;
+
+    tf::TransformListener* tf_listener;
+
     void globalMapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg) {
         global_map = *msg;
         global_resolution = msg->info.resolution;
@@ -45,13 +71,30 @@ private:
         current_pose = *msg;
     }
 
-    void publishLocalMap() {
+    void publishLocalMapInMapFrame() {
         if (!global_map.data.empty() && current_pose.header.stamp != ros::Time(0)) {
-            double robot_x = current_pose.pose.position.x;
-            double robot_y = current_pose.pose.position.y;
-            tf::Quaternion quat(current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z, current_pose.pose.orientation.w);
+            // Get the transform from the robot frame to the map frame
+            tf::StampedTransform transform;
+            try {
+                tf_listener->lookupTransform(map_frame, robot_frame, ros::Time(0), transform);
+            } catch (tf::TransformException& ex) {
+                ROS_WARN("%s", ex.what());
+                return;
+            }
+
+            double robot_x_map_frame = transform.getOrigin().x();
+            double robot_y_map_frame = transform.getOrigin().y();
+
             double roll, pitch, yaw;
-            tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+            transform.getBasis().getRPY(roll, pitch, yaw);
+
+            // Offset in front of the robot (0.15 meters in the direction of yaw)
+            double offset_x = control_point_offset * cos(yaw);
+            double offset_y = control_point_offset * sin(yaw);
+
+            // Adjust the position of the local map center
+            robot_x_map_frame += offset_x;
+            robot_y_map_frame += offset_y;
 
             double map_origin_x = global_map.info.origin.position.x;
             double map_origin_y = global_map.info.origin.position.y;
@@ -59,13 +102,13 @@ private:
             int map_height = global_map.info.height;
 
             nav_msgs::OccupancyGrid local_map;
-            local_map.header.frame_id = "base_link";
+            local_map.header.frame_id = map_frame;
             local_map.header.stamp = ros::Time::now();
             local_map.info.resolution = local_resolution;
             local_map.info.width = width_cells;
             local_map.info.height = height_cells;
-            local_map.info.origin.position.x = -(width_cells * local_resolution) / 2.0;
-            local_map.info.origin.position.y = -(height_cells * local_resolution) / 2.0;
+            local_map.info.origin.position.x = robot_x_map_frame - (width_cells * local_resolution) / 2.0;
+            local_map.info.origin.position.y = robot_y_map_frame - (height_cells * local_resolution) / 2.0;
             local_map.info.origin.position.z = 0.0;
             local_map.info.origin.orientation.w = 1.0;
 
@@ -75,10 +118,9 @@ private:
                 for (int x = 0; x < width_cells; ++x) {
                     double dx = (x - width_cells / 2) * local_resolution;
                     double dy = (y - height_cells / 2) * local_resolution;
-                    double rotated_dx = dx * cos(yaw) - dy * sin(yaw);
-                    double rotated_dy = dx * sin(yaw) + dy * cos(yaw);
-                    int global_x = static_cast<int>((robot_x + rotated_dx - map_origin_x) / global_resolution);
-                    int global_y = static_cast<int>((robot_y + rotated_dy - map_origin_y) / global_resolution);
+
+                    int global_x = static_cast<int>((robot_x_map_frame + dx - map_origin_x) / global_resolution);
+                    int global_y = static_cast<int>((robot_y_map_frame + dy - map_origin_y) / global_resolution);
 
                     if (global_x >= 0 && global_x < map_width && global_y >= 0 && global_y < map_height) {
                         int global_index = global_y * map_width + global_x;
@@ -91,23 +133,10 @@ private:
             local_map_pub.publish(local_map);
         }
     }
-
-    ros::Subscriber global_map_sub;
-    ros::Subscriber pose_sub;
-    ros::Publisher local_map_pub;
-
-    nav_msgs::OccupancyGrid global_map;
-    geometry_msgs::PoseStamped current_pose;
-    double global_resolution;
-    int width_cells, height_cells;
-
-    std::string global_map_topic, local_map_topic, pose_topic;
-    double width_meters, height_meters, local_resolution;
-    int publish_rate;
 };
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "local_map_node");
-    LocalMapNode localMapNode;
+    ros::init(argc, argv, "local_map_in_map_frame_node");
+    LocalMapInMapFrameNode localMapInMapFrameNode;
     return 0;
 }
